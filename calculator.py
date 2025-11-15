91a3ef6b5117d60470cf5ea90bb10d5f553921da
Brandon Tran <patso@Brandons-MacBook-Air-2.local>
2025-11-15 18:42:21 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -5,7 +5,31 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Division by zero")
+
+    return b / a
+
+def logarithm(a, b):
+    if a == 0:
+        raise ZeroDivisionError("log 0 is undefined")
+    elif a < 0:
+        raise ValueError("log of a negative number is undefined")
+
+    return math.log(b, a)
 
 
+def exponent(a, b):
+    return a**b
