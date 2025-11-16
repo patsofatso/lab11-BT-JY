56d65e27808ca9607c81a7500a02c3f3757cf7e9
Brandon Tran <patso@Brandons-MacBook-Air-2.local>
2025-11-15 20:00:24 -0500

    what the hell am i doing

--- calculator.py
+++ calculator.py
@@ -22,9 +22,6 @@ def hypotenuse(a, b):
     return math.hypot(a, b)
 
 def add(a, b):
-<<<<<<< HEAD
-=======
->>>>>>> bcd8e835dfbe1538f6739fb404e7d95921d6f892
     return a + b
 
 def subtract(a, b):
@@ -39,31 +36,12 @@ def divide(a, b):
     return b / a
 
 def logarithm(a, b):
-<<<<<<< HEAD
     if a == 0 or b == 0:
         raise ValueError("outside domain")
     elif a < 0 or b < 0:
         raise ValueError("outside domain")
-=======
 
 
 
 def exponent(a, b):
-<<<<<<< HEAD
-=======
-    return a**b
-
-import math
-
-def add(a, b): 
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def exponent(a, b):
->>>>>>> bcd8e835dfbe1538f6739fb404e7d95921d6f892
     return a**b
